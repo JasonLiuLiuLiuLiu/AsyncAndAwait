b3462b0c2829d29afb7c51558eb901d406c3f197
Liu ZhenYu <liuzhenyu@xinhehui.com>
2018-07-18 10:28:02 +0800

    异步方法和同步方法对比

---
+++ Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            var  pro=new  Program();
+            Console.WriteLine("index:"+pro.Index());
+            Console.WriteLine("indexAsync:"+pro.IndexAsync().Result);
+            Console.ReadKey();
+        }
+
+        public long Index()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ContentManagement service = new ContentManagement();
+            var content = service.GetContent();
+            var count = service.GetCount();
+            var name = service.GetName();
+
+            watch.Stop();
+            return watch.ElapsedMilliseconds;
+        }
+
+
+        public async Task<long> IndexAsync()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ContentManagement service = new ContentManagement();
+            var contentTask = service.GetContentAsync();
+            var countTask = service.GetCountAsync();
+            var nameTask = service.GetNameAsync();
+
+            var content = await contentTask;
+            var count = await countTask;
+            var name = await nameTask;
+            watch.Stop();
+            return watch.ElapsedMilliseconds;
+        }
+    }
+}
